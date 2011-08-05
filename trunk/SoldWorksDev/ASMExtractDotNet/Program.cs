7971ce023e6bb4d5eb84d70b8ce706813fc69428
alex890714@gmail.com <alex890714@gmail.com@0dedd60c-d7ff-c033-013f-1c5e0fd79d1a>
2011-08-05 16:43:55 +0000

    Finished Mate Extrction.

--- trunk/SoldWorksDev/ASMExtractDotNet/Program.cs
+++ trunk/SoldWorksDev/ASMExtractDotNet/Program.cs
@@ -39,26 +39,68 @@ namespace ASMExtractDotNet {
             string sPadStr = "";
             long i = 0;
 
-            for (i = 0; i < nLevel; i++) {
+            for (i = 0; i <= nLevel; i++) {
                 sPadStr = sPadStr + "\t";
+            }
+
+            // To extract mate infomation.
+            if (swFeat.GetTypeName2().IndexOf("Mate") != -1 && swFeat.GetTypeName2() != "MateGroup") {
+                Debug.Print("[------------MateInfo:" + swFeat.Name + "[" + swFeat.GetTypeName2() + "]------------]");
+                Mate2 swMate = swFeat.GetSpecificFeature2();
+                if (swMate != null) {
+                    Debug.Print("Aligment:" + swMate.Alignment);
+                    Debug.Print("CanBeFlipped:" + swMate.CanBeFlipped);
+                    Debug.Print("Type:" + swMate.Type);
+                    Debug.Print("MaximumVariation:" + swMate.MaximumVariation);
+                    Debug.Print("MinimumVariation:" + swMate.MinimumVariation);
+
+                    Debug.Print("Entities:");
+                    //To output entities.
+                    int mateEntityCount = 0;
+                    MateEntity2[] entities = null;
+                    mateEntityCount = swMate.GetMateEntityCount();
+                    Face2[] supplementalFaces = null;
+                    entities = new MateEntity2[mateEntityCount];
+                    for (int j = 0; j < mateEntityCount; ++j) {
+                        entities[j] = swMate.MateEntity(j);
+                        Debug.Print("\tEntity[" + j + "] Name:" + entities[j].ReferenceComponent.Name2);
+                        Debug.Print("\t\tEntity Params Size:" + entities[j].GetEntityParamsSize());
+                        Debug.Print("\t\tLocation[x, y, z]:" + (double)entities[j].EntityParams[0] + ","
+                            + (double)entities[j].EntityParams[1] + "," + (double)entities[j].EntityParams[2]);
+                        Debug.Print("\t\tV1,V2,V3:" + (double)entities[j].EntityParams[3] + ","
+                            + (double)entities[j].EntityParams[4] + "," + (double)entities[j].EntityParams[5]);
+                        Debug.Print("\t\tR1,R2:" + (double)entities[j].EntityParams[6] + ","
+                            + (double)entities[j].EntityParams[7]);
+                        supplementalFaces = (Face2[])swMate.GetSupplementalFaces(j);
+                    }
+                }
             }
 
             Debug.Print(sPadStr + swFeat.Name + " [" + swFeat.GetTypeName2() + "]");
             if ((swSubFeat = swFeat.IGetFirstSubFeature()) != null) {
                 TraverseFeatures2(swSubFeat, nLevel + 1);
             }
-            if ((swFeat = swFeat.IGetNextFeature()) != null) {
-                TraverseFeatures2(swFeat, nLevel);
+
+            if (nLevel == 1) {
+                if ((swFeat = swFeat.IGetNextFeature()) != null) {
+                    TraverseFeatures2(swFeat, nLevel);
+                }
+            } else {
+                if ((swFeat = swFeat.GetNextSubFeature()) != null) {
+                    TraverseFeatures2(swFeat, nLevel);
+                }
             }
+            
         }
+        
         
         public void TraverseFeatures(Feature swFeat, long nLevel) {
             Feature swSubFeat;
-            string sPadStr = " ";
+            string sPadStr = "";
             long i = 0;
 
             for (i = 0; i <= nLevel; i++) {
-                sPadStr = sPadStr + " ";
+                sPadStr = sPadStr + "\t";
             }
 
             while ((swFeat != null)) {
@@ -88,11 +130,11 @@ namespace ASMExtractDotNet {
             object[] vChildComp;
             Component2 swChildComp;
 
-            string sPadStr = " ";
+            string sPadStr = "";
             long i = 0;
 
-            for (i = 0; i <= nLevel - 1; i++) {
-                sPadStr = sPadStr + " ";
+            for (i = 0; i < nLevel; i++) {
+                sPadStr = sPadStr + "\t";
             }
 
             vChildComp = (object[])swComp.GetChildren();
@@ -108,7 +150,8 @@ namespace ASMExtractDotNet {
         public void TraverseModelFeatures(ModelDoc2 swModel, long nLevel) {
             Feature swFeat;
             swFeat = (Feature)swModel.FirstFeature();
-            TraverseFeatures(swFeat, nLevel);
+            //TraverseFeatures(swFeat, nLevel);
+            TraverseFeatures2(swFeat, nLevel);
         }
 
         public void Run() {
@@ -126,11 +169,13 @@ namespace ASMExtractDotNet {
             myStopwatch.Start();
             Debug.Print("File = " + swModel.GetPathName());
 
-            //TraverseModelFeatures(swModel, 1);
+            TraverseModelFeatures(swModel, 1);
 
+            /*
             if (swModel.GetType() == (int)swDocumentTypes_e.swDocASSEMBLY) {
                 TraverseComponent(swRootComp, 1);
             }
+             * */
 
             myStopwatch.Stop();
             TimeSpan myTimespan = myStopwatch.Elapsed;
