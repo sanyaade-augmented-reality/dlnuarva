e201660b929eb73d9c1a62ba966c231bbd7bd752
alex890714@gmail.com <alex890714@gmail.com@0dedd60c-d7ff-c033-013f-1c5e0fd79d1a>
2011-11-15 06:28:19 +0000

    AR包调试好了

---
+++ VAAR/AR/AR.cpp
@@ -0,0 +1,9 @@
+#include "AR.h"
+
+namespace aruco {
+
+__declspec(dllexport) void AR::Test() {
+	// do nothing;
+}
+
+} // namespcae aruco
---
+++ VAAR/AR/AR.h
@@ -0,0 +1,12 @@
+#ifndef AR_AR_H_
+#define AR_AR_H_
+
+namespace aruco {
+
+class AR {
+public:
+	__declspec(dllexport) void Test();
+}; // class AR
+
+} // namespace aruco
+#endif
